7f74f572e609e201df99a0edee0f49eaec72bd2b
David Seis <david.seis.sec@gmail.com>
2025-05-20 19:35:13 -0400

    Add PowerShell scripts for SQL Server maintenance and automation tasks
    - Created script to remove maintenance procedures and jobs from the SQL Server instance.
    - Added script to create an output directory for automation tasks.
    - Developed script to export instance tracking data to a CSV file.
    - Implemented script to load data from the CSV into the SQL Server database.
    - Set up environment variables for automation scripts.
    - Created a script for silent installation of SQL Server Management Studio (SSMS).
    - Developed a task scheduler script to automate job run reporting and email notifications.
    - Added script to set up a local user for task scheduling demos.

---
+++ Sessions/Intro to PowerShell Automation for the MSSQL DBA/1 - History/2025-05-20 - TFAB/Scripts/2 - Basic Automations/2 - Backup a Database.ps1
@@ -0,0 +1,24 @@
+
+Set-DbatoolsConfig -FullName sql.connection.trustcert -Value $true -register  
+
+# Targets and other variables
+    $SQLInstance =  "Seis-Work" 
+
+    $date = Get-Date -UFormat "%Y-%m-%d (%H.%M)"
+
+    $backuppath = "C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\Backup\"
+
+    $database = "DB_Administration"
+#backup a database via DBAtools
+    Invoke-DBAQuery -sqlinstance $SQLInstance   -query  "
+    BACKUP DATABASE [$database] 
+    TO  DISK = N'$($backuppath+$database)`_$($date).bak' 
+    WITH    NOFORMAT
+        ,   NOINIT
+        ,   NAME = N'$database-Full Database Backup - $date'
+        ,   SKIP
+        ,   NOREWIND
+        ,   NOUNLOAD
+        ,   STATS = 10
+    GO
+    "
---
+++ Sessions/Intro to PowerShell Automation for the MSSQL DBA/1 - History/2025-05-20 - TFAB/Scripts/2 - Basic Automations/4 - Check Job Commands.ps1
@@ -0,0 +1,24 @@
+
+<# Query to check the current job commands #>
+
+    $Target = 'Seis-Work'
+
+    Set-DbatoolsConfig -FullName sql.connection.trustcert -Value $true
+
+
+    $results = Invoke-dbaquery -SQlinstance $Target   -query "
+    select 
+        @@servername as [Server Name]
+        , s.name AS JobName
+        , js.command 
+        , SERVERPROPERTY('InstanceDefaultBackupPath') AS [Default Backup Path]
+        
+    from msdb.dbo.sysjobs s
+    left join msdb.dbo.sysjobsteps js on js.job_id = s.job_id
+    where s.name like '%MAINT%DatabaseBackup%'
+    ORDER by s.name
+
+    " 
+
+    $results | Out-GridView 
+    $results | Format-Table
