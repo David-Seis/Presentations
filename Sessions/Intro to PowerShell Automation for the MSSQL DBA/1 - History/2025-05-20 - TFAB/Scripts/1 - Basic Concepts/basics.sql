36c309be91bfa5f0c9f4a45fc58dfbaf21930bdc
David Seis <david.seis.sec@gmail.com>
2025-05-20 15:32:19 -0400

    Revert "Refactor code structure and remove redundant sections for improved readability and maintainability"
    This reverts commit e394e2e7d15dc318f375bf08184d3b7ad5d14bad.

--- Sessions/Intro to PowerShell Automation for the MSSQL DBA/1 - History/2025-05-20 - TFAB/Scripts/1 - Basic Concepts/basics.sql
+++
@@ -1,5 +0,0 @@
-SELECT  @@Servername as Servername,
-        @@Version as Version,
-        @@Language as Language,
-        @@DateFirst as DateFirst,
-        @@OPTIONS as Options
--- Sessions/Intro to PowerShell Automation for the MSSQL DBA/1 - History/2025-05-20 - TFAB/Scripts/3 - Advanced Automation/SETUP - local user.ps1
+++
@@ -1,8 +0,0 @@
-$Password = Read-Host -AsSecureString
-$params = @{
-    Name        = 'TaskUser'
-    Password    = $Password
-    FullName    = 'Task User'
-    Description = 'Local user for task scheduler demos'
-}
-New-LocalUser @params
