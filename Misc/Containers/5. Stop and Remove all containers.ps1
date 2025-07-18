840bc411ea53bf63fc8024610bef53b4cde24a3f
David Seis <50645790+seriousd6@users.noreply.github.com>
2025-07-17 23:02:06 -0400

    Refactor container scripts: streamline SQL container creation and enhance removal process

--- Misc/Containers/5. Stop and Remove all containers.ps1
+++ Misc/Containers/5. Stop and Remove all containers.ps1
@@ -1,9 +1,23 @@
+Set-Location -Path "C:\Users\DavidSeis\Documents\Git\Presentations\Misc\Containers"
+
+
 $containers = docker ps -a --format "{{.Names}}"
 
+
+
+#Partial Teardown - Just containers
 foreach ($container in $containers) {
     Docker stop $container | Out-Null
-    Docker rm $container  
+    Docker rm $container | Out-Null
 
-    Get-item -path ".\$container" | Remove-Item
+    Write-Host "Removed Container : $container" -ForegroundColor Green
 }
 
+#Full Teardown - Containers and directories
+foreach ($container in $containers) {
+    Docker stop $container | Out-Null
+    Docker rm $container | Out-Null
+
+    Get-item -path ".\$container" | Remove-Item -Recurse -Force
+    Write-Host "Removed Container and Directory: $container" -ForegroundColor Green
+}
